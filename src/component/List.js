e39b58f386debfb823e71431ea682a3f61f67e3d
Akash981616 <Ak98161615@gmail.com>
2022-11-02 18:43:05 +0530

    first commit

---
+++ src/component/List.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./List.scss";
+import userEditIcon from "../assets/userEditIcon.svg";
+import userDeleteIcon from "../assets/userDeleteIcon.svg";
+const List = ({ name, phoneno, property, email, document, city, header }) => {
+  return (
+    <div className={header ? "list-container" : "list-container-list"}>
+      <div className="list-name-container">
+        <h4>{name}</h4>
+      </div>
+      <div
+        className={
+          header ? "list-phone-container" : "list-phone-container-list"
+        }
+      >
+        <h4>{phoneno}</h4>
+      </div>
+      <div
+        className={
+          header ? "list-email-container" : "list-email-container-list"
+        }
+      >
+        <h4>{email}</h4>
+      </div>
+      <div
+        className={
+          header ? "list-property-container" : "list-property-container-list"
+        }
+      >
+        <h4>{property}</h4>
+      </div>
+      <div
+        className={
+          header ? "list-property-container" : "list-city-container-list"
+        }
+      >
+        <h4>{city}</h4>
+      </div>
+      <div
+        className={
+          header ? "list-document-container" : "list-document-container-list"
+        }
+      >
+        <h4>{document}</h4>
+      </div>
+
+      <div className="list-action-container">
+        {header ? (
+          <h4>Action</h4>
+        ) : (
+          <div class="userPage-curd-operation">
+            <div class="userPage-curd-operation-div">
+              <img src={userEditIcon} alt="error"></img>
+            </div>
+            <div class="userPage-curd-operation-div-white">
+              <img src={userDeleteIcon} alt="error"></img>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default List;
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
-import "./UserPage.css";
+import UserSearchIcon from "../assets/UserSearchIcon.svg";
 import TopNavBar from "../component/TopNavBar";
+import DownArrow from "../assets/downArrow.svg";
+import "./UserPage.scss";
+import plusCircleICon from "../assets/plusCircle.svg";
+import List from "../component/List";
+import {data} from "../data/listData.js"
 const UserPage = () => {
-    const selectStyle = {
-      backgroundColor: "#8A8A8A",
-      borderRadius: "4px 4px 0px 0px",
-      color: "#FFFFFF"
-    };
-    const [selectOptiom, setSelectOptiom] = useState("Ownwer");
+ 
+  const selectStyle = {
+    backgroundColor: "#8A8A8A",
+    borderRadius: "4px 4px 0px 0px",
+    color: "#FFFFFF",
+  };
+  const [selectOptiom, setSelectOptiom] = useState("Ownwer");
   return (
     <div>
       <TopNavBar />
@@ -40,9 +46,45 @@ const UserPage = () => {
         </div>
       </div>
       <div className="userpage-top-conatiner-utility">
-        <h1>OnwerList</h1>
-        <div className="userpage-search-filter-container">  
+        <h1>Onwer List</h1>
+        <div className="userpage-search-filter-container">
+          <div className="userpage-search-container">
+            <input type="text" placeholder="Search"></input>
+            <img src={UserSearchIcon} alt="error"></img>
+          </div>
+          <div className="userpage-filter-container">
+            <h3>Filter</h3>
+            <img src={DownArrow} alt="error"></img>
+          </div>
+          <button className="create-user-button">
+            <img src={plusCircleICon} alt="error"></img> <h3>Create User</h3>
+          </button>
         </div>
+      </div>
+      <div style={{ marginTop: "40px" }}>
+        <List
+          name={"Name"}
+          phoneno={"Phone no."}
+          property={"Property"}
+          email={"Email"}
+          city={"City"}
+          document={"Docment"}
+          header={true}
+        />
+      </div>
+      <div className="userpage-list-container">
+        {data.map((e) => {
+          return (
+            <List key={e.id}
+              name={e.name}
+              phoneno={e.phoneno}
+              property={e.property}
+              email={e.email}
+              city={e.city}
+              document={e.document}
+            />
+          );
+        })}
       </div>
     </div>
   );
